4ad2336e2e4f15f3b863770a8482c02faf1e1b6c
daranega <daranega@optimusprice.ai>
2023-12-13 19:13:48 +0100

    refactored the login component

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -2,25 +2,10 @@ import { useState, useEffect, useRef } from 'react'
 import Blogs from './components/Blogs'
 import Login from './components/Login'
 import Notification from './components/Notification'
-import Togglable from './components/Togglable'
-import blogService from './services/blogs'
-import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [notification, setNotification] = useState({ message: '', tone: null })
-
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      blogService.setToken(user.token)
-    }
-  }, [])
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -29,41 +14,16 @@ const App = () => {
     return () => clearTimeout(timer)
   }, [notification])
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    const auth = await loginService.login({ username, password })
-    if (auth) {
-      setUser(auth)
-      blogService.setToken(auth.token)
-      window.localStorage.setItem(
-        'loggedUser', JSON.stringify(auth)
-      )
-      setNotification({ message: 'correclty logged in', tone: 'good' })
-    } else {
-      setNotification({ message: 'incorrect username or password', tone: 'bad' })
-    }
-  }
-
   const handleLogout = () => {
     setUser(null)
-    setPassword('')
     window.localStorage.removeItem('loggedUser')
-  }
-
-  const loginForm = () => {
-    return (
-      <Togglable buttonLabel='login'>
-        <Login handleLogin={handleLogin} setPassword={setPassword}
-        setUsername={setUsername} username={username} password={password} />
-      </Togglable>
-    )
   }
 
   return (
     <div>
       <Notification notification={notification}/>
       <h2>blogs</h2>
-      {!user && loginForm()}
+      {!user && <Login setNotification={setNotification} setUser={setUser}/>}
       {user && 
       <div>
         logged in as {user.name} <button onClick={handleLogout}>logout</button>
--- bloglist-frontend/src/components/Login.jsx
+++ bloglist-frontend/src/components/Login.jsx
@@ -1,26 +1,63 @@
-const Login = ({ setUsername, setPassword, handleLogin, username, password }) => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  )
+import { useState, useEffect } from 'react'
+import Togglable from './Togglable'
+import loginService from '../services/login'
+import blogService from '../services/blogs'
+
+const Login = ({ setNotification, setUser }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    const auth = await loginService.login({ username, password })
+    if (auth) {
+      setUser(auth)
+      blogService.setToken(auth.token)
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(auth)
+      )
+      setNotification({ message: 'correctly logged in', tone: 'good' })
+      setPassword('')
+    } else {
+      setNotification({ message: 'incorrect username or password', tone: 'bad' })
+    }
+  }
+
+    return (
+      <Togglable buttonLabel='login'>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </Togglable>
+    )
+}
 
   export default Login
   
